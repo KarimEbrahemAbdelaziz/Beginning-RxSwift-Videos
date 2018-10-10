506c97f55faa5ceaa8a9fe556d04c95f8b8ba635
Karim Ebrahem <karimabdelazizmansour@gmail.com>
2018-10-11 00:35:15 +0200

    Code for creating observables lesson

--- GettingStartedRxSwift/GettingStartedRxSwift/GettingStartedRxSwift.playground/Contents.swift
+++ GettingStartedRxSwift/GettingStartedRxSwift/GettingStartedRxSwift.playground/Contents.swift
@@ -1,2 +1,14 @@
 
+import RxSwift
 
+example(of: "Create Observables") {
+    
+    let mostPopular = Observable<String>.just(episodeV)
+    
+    let originalTrilogy = Observable.of(episodeI, episodeV, episodeVI)
+    
+    let prequalTrilogy = Observable.of([episodeI, episodeV, episodeVI])
+    
+    let sequalTrilogy = Observable.from([episodeI, episodeV, episodeVI])
+
+}
