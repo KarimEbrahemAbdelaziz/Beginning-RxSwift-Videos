d958a85afaa40ac6c19f989db0547ab5b6cee4cd
Karim Ebrahem <karimabdelazizmansour@gmail.com>
2018-10-11 00:28:19 +0200

    Add project structure and helper class

--- GettingStartedRxSwift/GettingStartedRxSwift/ViewController.swift
+++ GettingStartedRxSwift/GettingStartedRxSwift/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print(Observable.of("Karim").asObservable())
     }
 
 
